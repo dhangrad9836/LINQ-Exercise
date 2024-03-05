124b7ef7e07312c51c12f28a869ea9f99525c391
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-03-04 22:56:53 -0500

    Completed LINQ exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //create a list
+            List<string> videoGames = new List<string>();
+
+            //populate the list with video games
+            videoGames.Add("Super Mario Bros");
+            videoGames.Add("Pac Man");
+            videoGames.Add("Resident Evil");
+            videoGames.Add("Eleven Table Tennis");
+            videoGames.Add("Street Fighter");
+
+            //order the list of games by the length of the game name
+            videoGames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
+
+            //foreach (var game in videoGames)
+            //{
+            //    Console.WriteLine(game);
+            //}
+
+            Console.ReadLine();
         }
     }
 }
